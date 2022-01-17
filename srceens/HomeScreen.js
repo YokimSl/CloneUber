9e8ffd48b8ffd5d8b1125c08d0570b14fef9e808
YokimSl <jhonnata1337@gmail.com>
2022-01-17 17:44:17 -0300

    fisrt commit

---
+++ srceens/HomeScreen.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { StyleSheet, Text, View, SafeAreaView  } from "react-native";
+import tw from 'twrnc';
+
+const HomeScreen = () => {
+    return( 
+        <SafeAreaView >
+            <Text>Eu sou a tela Inicial </Text>
+        </SafeAreaView>
+    );
+};
+
+export default HomeScreen;
+
+const styles = StyleSheet.create({
+    text:{
+        color: "blue",
+    }
+
+});
+  
+
